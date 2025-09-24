91bb71c9a01baac0983cbf13713c2d090156bbe0
K4SUND <kasundilumina2016@gmail.com>
2025-09-25 01:00:04 +0530

    first video

---
+++ src/main/java/com/springlearn/psr_vlog/PsrVlogApplication.java
@@ -0,0 +1,13 @@
+package com.springlearn.psr_vlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsrVlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsrVlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springlearn/psr_vlog/PsrVlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springlearn.psr_vlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsrVlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
